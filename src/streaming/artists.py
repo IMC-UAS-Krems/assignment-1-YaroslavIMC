88b6df81d5177a5741465e7cc39d65c501790e56
Yaroslav Kostanda <25IMC10963@fh-krems.ac.at>
2026-04-07 13:51:22 +0200

    prog

--- src/streaming/artists.py
+++ src/streaming/artists.py
@@ -8,15 +8,15 @@ Classes to implement:
 """
 
 class Artist:
-    def __init__(self, artist_id: str, name: str, genre: str) -> None:
+    def __init__(self, artist_id: str, name: str, genre: str):
         self.artist_id = artist_id
         self.name = name
         self.genre = genre
         self.tracks = []
 
-    def add_track(self, track) -> None:
+    def add_track(self, track):
         if track not in self.tracks:
             self.tracks.append(track)
 
-    def track_count(self) -> int:
+    def track_count(self):
         return len(self.tracks)
